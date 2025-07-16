d48049724c543596dad3656ce2f4b41b2396cadd
davidtuning <tuninggamer@gmail.com>
2025-07-16 17:04:50 -0400

    Update meta.go
    Signed-off-by: davidtuning <tuninggamer@gmail.com>

--- drivers/terabox/meta.go
+++ drivers/terabox/meta.go
@@ -7,16 +7,19 @@ import (
 
 type Addition struct {
 	driver.RootPath
-	Cookie string `json:"cookie" required:"true"`
+	Cookie string `json:"cookie" required:"true" help:"Terabox session cookie (get from browser developer tools)"`
 	//JsToken        string `json:"js_token" type:"string" required:"true"`
-	DownloadAPI    string `json:"download_api" type:"select" options:"official,crack" default:"official"`
-	OrderBy        string `json:"order_by" type:"select" options:"name,time,size" default:"name"`
-	OrderDirection string `json:"order_direction" type:"select" options:"asc,desc" default:"asc"`
+	UserAgent      string `json:"user_agent" required:"true" default:"terabox;1.40.0.132;PC;PC-Windows;10.0.26100;WindowsTeraBox" help:"User agent string for API requests"`
+	DownloadAPI    string `json:"download_api" type:"select" options:"official,crack" default:"official" help:"API method for downloading files"`
+	OrderBy        string `json:"order_by" type:"select" options:"name,time,size" default:"name" help:"Default sorting field for file listings"`
+	OrderDirection string `json:"order_direction" type:"select" options:"asc,desc" default:"asc" help:"Default sorting direction"`
+	UploadThreads  int    `json:"upload_threads" type:"number" default:"2" help:"Number of parallel threads for uploads (1-10, default: 2)"`
+	RetryCount     int    `json:"retry_count" type:"number" default:"5" help:"Number of retry attempts for failed operations (1-10, default: 3)"`
 }
 
 var config = driver.Config{
-	Name:        "Terabox",
-	DefaultRoot: "/",
+	Name:              "Terabox",
+	DefaultRoot:       "/",
 }
 
 func init() {
